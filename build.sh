bfc58cc0a014c02b20ee2b10360c7b6b48b87ce9
marcohald <marcohald97@gmail.com>
2023-10-25 22:51:41 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/ash
 cd src
 go get github.com/eclipse/paho.mqtt.golang
-go build -ldflags "-s -w" src/cec-client-mqtt-bridge.go
+go build -ldflags "-s -w" cec-client-mqtt-bridge.go
 cp cec-client-mqtt-bridge ../
